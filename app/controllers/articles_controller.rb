30d17d0458a5fc0e8ebeaf96acd8e7be75d9ca98
uzair ajmal <uzairajmal398@gmail.com>
2020-08-26 13:11:20 +0500

    getting live

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,15 @@
 class ArticlesController < ApplicationController
 
+before_action :set_article , only: [:edit ,:update ,:destroy]
+ 
+ def index
+
+ @article=Article.all
+ 
+ end
+
+
+
  def new
  @article=Article.new
  end
@@ -8,21 +18,44 @@ class ArticlesController < ApplicationController
  @article=Article.new(article_params)
  @article.save
 
-  if @article.save
+   if @article.save
  	flash[:notice] ="Article Created"
  	redirect_to article_path(@article)
-  else
+   else
     render 'new'
- 
-  end
+     end
+ end
+
+ def edit
 
  end
+
+ def update
+  if @article.update(article_params)
+           flash[:notice]="Edited Success"
+           redirect_to article_path(@article)
+    else
+         render 'edit'               
+	end
+  end
+
+ def destroy
+ @article.destroy
+ flash[:notice] =" Successfully Deleted"
+ redirect_to articles_path 
+ end
+
 def show
 
-@article=Article.find(params[:id])
-
+ @article=Article.find(params[:id])
 end
- private
+   private
+
+   def set_article
+   @article=Article.find(params[:id]) 
+
+   	
+   end
  def article_params
  params.require(:article).permit(:titile, :description)
 
